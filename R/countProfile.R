7e1808b6de3464a61f3f8d08d6222fada6b6d8f4
ALentini <antonio.lentini@liu.se>
2018-06-05 14:20:18 +0200

    Update countProfile.R

--- R/countProfile.R
+++ R/countProfile.R
@@ -1,5 +1,5 @@
 #Get meta profile for alignments centered around bed feature
-get.prof <- function(bed,aln,size=2001,n=101){
+countProfile <- function(bed,aln,size=2001,n=101){
   require(GenomicRanges)
   bin <- unlist(tile(resize(bed,width = size,fix = "center"),width = size/n))
   cnt <- countOverlaps(bin,aln) / (length(aln) / 1e6)
